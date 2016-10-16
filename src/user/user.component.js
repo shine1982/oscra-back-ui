4a7105dfcb7f4f374c291cd7f25cdb65c7474683
nicolas2lee <nicolas2lee@gmail.com>
2016-10-16 22:46:49 +0200

    clean code

--- src/user/user.component.js
+++ src/user/user.component.js
@@ -7,7 +7,6 @@ angular.module('oscra-ui.user').component('userinfo', {
     },
     template: require('./componentTemplate/userAllInfo.html'),
     controller: function userInfoController(){
-        //console.log(this.user)
     }
 })
 .component('mdTable',{
@@ -20,17 +19,9 @@ angular.module('oscra-ui.user').component('userinfo', {
         thumbs:'=',
         count: '='
     },
-    //template: angular.element(document.querySelector('#md-table-template')).html(),
     template: require('./componentTemplate/tableTemplate.html'),
     controller: function mdTableController($scope,$filter) {
         var vm=this;
-        console.log('headers '+vm.headers[0].field);
-        console.log('content '+vm.content);
-        console.log('sortable '+vm.sortable[0]);
-        console.log('filters '+vm.filters);
-        console.log('custom-class '+vm.customClass);
-        console.log('thumbs '+vm.thumbs);
-        console.log('count '+vm.count);
 
          var orderBy = $filter('orderBy');
          vm.tablePage = 0;
@@ -51,26 +42,6 @@ angular.module('oscra-ui.user').component('userinfo', {
          vm.goToPage = function (page) {
             vm.tablePage = page;
          }
-        /*
-        var orderBy = $filter('orderBy');
-        $scope.tablePage = 0;
-        $scope.nbOfPages = function () {
-            return Math.ceil($scope.content.length / $scope.count);
-        },
-            $scope.handleSort = function (field) {
-                if ($scope.sortable.indexOf(field) > -1) { return true; } else { return false; }
-            };
-        $scope.order = function(predicate, reverse) {
-            $scope.content = orderBy($scope.content, predicate, reverse);
-            $scope.predicate = predicate;
-        };
-        $scope.order($scope.sortable[0],false);
-        $scope.getNumber = function (num) {
-            return new Array(num);
-        };
-        $scope.goToPage = function (page) {
-            $scope.tablePage = page;
-        }*/
     }
 
 })
