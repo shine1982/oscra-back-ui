d18b1c986b5dfa21bc7a258d34b14c8799577628
nicolas2lee <nicolas2lee@gmail.com>
2016-11-22 10:55:40 +0100

    update server.js to depoy

--- serve.js
+++ serve.js
@@ -2,12 +2,12 @@ var config = require("./webpack.config.js");
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
 var port = process.env.PORT || 8000;
-config.entry.app.unshift("webpack-dev-server/client?http://80.12.83.208:"+port);
+config.entry.app.unshift("webpack-dev-server/client?http://localhost:"+port);
 var compiler = webpack(config);
 var server = new WebpackDevServer(compiler, {
     proxy: {
         '/api/*': {
-            target: 'http://80.12.83.208:8080',
+            target: 'http://localhost:8080',
             secure: false
         }
     }
